5237318d2823e64f7b4b02131f71931b4f8a5870
XuHaizi <xuhaizi@zwcad.com>
2023-01-19 13:57:09 +0800

    fix

--- src/preload.js
+++ src/preload.js
@@ -2,13 +2,12 @@
 
 // 所有的 Node.js API接口 都可以在 preload 进程中被调用.
 // 它拥有与Chrome扩展一样的沙盒。
-window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
 
-  for (const dependency of ["chrome", "node", "electron"]) {
-    replaceText(`${dependency}-version`, process.versions[dependency]);
-  }
+const { contextBridge } = require("electron");
+
+contextBridge.exposeInMainWorld("versions", {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  // 能暴露的不仅仅是函数，我们还可以暴露变量
 });
